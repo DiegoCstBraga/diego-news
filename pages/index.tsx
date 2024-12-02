e2e08fdc6688b6e14e58960742be04859b093eae
Diego Braga <diego.cstbraga@gmail.com>
2024-12-02 23:25:32 +0100

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+      <h1>Para minha amada</h1>
+      <p>Te amo muito, meu amorzinho ❤️</p>
+
+      <p>De: Diego</p>
+      <p>Para: Nina, AKA amor da minha vida</p>
+    </>
+  );
+}
